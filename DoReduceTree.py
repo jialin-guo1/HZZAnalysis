7d70bfc7721f1098082773bbe2f7ba69bb75af76
jialin-guo1 <jialin.guo@cern.ch>
2020-12-23 22:44:01 +0800

    fix reduce tree

--- DoReduceTree.py
+++ DoReduceTree.py
@@ -12,12 +12,14 @@ def processCmd(cmd, quite = 0):
     else:
         return output
 
-keeplist  = "lep_id lep_pt lep_eta lep_phi lep_mass lepFSR_pt lepFSR_eta lepFSR_phi lepFSR_mass lep_Hindex lep_pterr lep_pterrold lep_ecalEnergy lep_isEB lep_isEE lep_tightId lep_Sip lep_RelIsoNoFSR passedFullSelection passedZ1LSelection  "keeplist  = "lep_id lep_pt lep_eta lep_phi lep_mass lepFSR_pt lepFSR_eta lepFSR_phi lepFSR_mass lep_Hindex lep_pterr lep_pterrold lep_ecalEnergy lep_isEB lep_isEE lep_tightId lep_Sip lep_RelIsoNoFSR passedFullSelection passedZ1LSelection passedZXCRSelection nZXCRFailedLeptons eventWeight mass4l k_ggZZ k_qqZZ_qcd_dPhi k_qqZZ_qcd_M k_qqZZ_ewk k_qqZZ_qcd_Pt crossSection EventCat Run Event LumiSect"
+keeplist  = "lep_id lep_pt lep_eta lep_phi lep_mass lepFSR_pt lepFSR_eta lepFSR_phi lepFSR_mass lep_Hindex lep_pterr lep_pterrold lep_ecalEnergy lep_isEB lep_isEE lep_tightId lep_Sip lep_RelIsoNoFSR passedFullSelection passedZ1LSelection passedZXCRSelection nZXCRFailedLeptons eventWeight mass4l k_ggZZ k_qqZZ_qcd_dPhi k_qqZZ_qcd_M k_qqZZ_ewk k_qqZZ_qcd_Pt crossSection EventCat Run Event LumiSect"
 filenamesInput = "/cms/user/guojl/Sample/2016_noDuplicates.root"
 filenameOutput = "/cms/user/guojl/Sample/skimed/2016_noDuplicates.root"
 tree = "passedEvents"
-directory = ""
+#directory = ""
 removelist = "*"
 
-cmd = "python reduceTree.py"+" "+filenamesInput+" "+filenameOutput+" "+"-t"+" "+tree+" "+"-d"+directory+" "+"-r"+" "+removelist+" "+"-k"+" "+keeplist
+#cmd = "python reduceTree.py"+" "+filenamesInput+" "+filenameOutput+" "+"-t"+" "+tree+" "+"-d"+directory+" "+"-r"+" "+removelist+" "+"-k"+" "+keeplist
+cmd = "python reduceTree.py"+" "+filenamesInput+" "+filenameOutput+" "+"-t"+" "+"\"%s\""%tree+" "+"-r"+" "+"\"%s\""%removelist+" "+"-k"+" "+"\"%s\""%keeplist
 print cmd
+processCmd(cmd)
